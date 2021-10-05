f1b292c998bad6b9c94404380b406972c69a61cb
taighlor326 <91159557+taighlor326@users.noreply.github.com>
2021-10-05 17:19:03 -0400

    Song.cs
    starting song.cs

---
+++ OBGAPI/OBGAPI/Data Models/song.cs
@@ -0,0 +1,17 @@
+using systems;
+using System.DateTime;
+
+
+namespace OBGAPI.DataModels
+{
+    public class Song {
+
+        private string id {get;set;}
+        private List<String> artists {get; set;}
+        private DateTime year
+        private Name {string}
+        private Studio {string}
+        private Link {string}
+
+    }
+}
